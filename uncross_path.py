c56d51df21ef646bc8ea3a67c7c3c0a3ca4bbe52
SPNSPN <kusamoti@gmail.com>
2020-09-07 20:43:55 +0900

    mod  uncross

--- uncross_path.py
+++ uncross_path.py
@@ -14,20 +14,25 @@ def parse_cmd_args ():
 def has_cross (pb, pe, qb, qe):
 	m = (pb[1] - pe[1]) / (pb[0] - pe[0])
 	c = pb[1] - m * pb[0] 
-	print("debug: hasc: {0}, {1}, {2}, {3}".format(pb, pe, qb, qe))
-	print("debug: yy: {0}, {1}".format(m * qb[0] + c - qb[1], m * qe[0] + c - qe[1]))
+#	print("debug: hasc: {0}, {1}, {2}, {3}".format(pb, pe, qb, qe))
+#	print("debug: yy: {0}, {1}".format(m * qb[0] + c - qb[1], m * qe[0] + c - qe[1]))
 	return ((m * qb[0] + c - qb[1]) * (m * qe[0] + c - qe[1])) < 0
 
+def has_cross_in_path (pb, pe, path):
+	for pathidx_ in range(len(path) - 1):
+		if has_cross(path[pathidx_], path[pathidx_ + 1], pb, pe):
+			return True
+	return False
+
 def uncross (rest, path):
+#	print("debug: path: {0}".format(path))
 	if not rest:
-		return path
+		if has_cross_in_path(path[0], path[-1], path):
+			return None
+		else:
+			return path
 	for ptidx in range(len(rest)):
-		crossing = False
-		for pathidx_ in range(len(path) - 1):
-			if has_cross(path[pathidx_], path[pathidx_ + 1], path[-1], rest[ptidx]):
-				crossing = True
-				break
-		if crossing:
+		if has_cross_in_path(path[-1], rest[ptidx], path):
 			continue
 		upath = uncross(rest[:ptidx] + rest[ptidx + 1:], path + [rest[ptidx]])
 		if upath:
